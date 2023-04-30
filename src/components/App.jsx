88d7d69da6331e1bb597c1951c4b7f91e613d88d
Игорь <bujhtyz228@gmail.com>
2023-04-30 12:37:20 +0300

    now

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ export const App = () => {
   return (
     <BrowserRouter basename="/">
       <Routes>
-        <Route path="/" element={<HomePage />} exact/>
+        <Route path="/" element={<HomePage />}/>
         <Route path="/tweets" element={<UserList />} />
       </Routes>
     </BrowserRouter>
